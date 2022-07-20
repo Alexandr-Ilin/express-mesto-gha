017193e5c0cde11f0d6e5b97ad188279a9203358
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-07-20 17:29:13 +0300

    updateUser5

--- controllers/users.js
+++ controllers/users.js
@@ -51,8 +51,8 @@ const getUserById = (req, res) => {
 }
 
 const updateUserProfile = (req, res) => {
-  //const { name, about } = req.body
-  User.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+  const { name, about } = req.body
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .then((user) => {
       // if (!user) {
       //   res.status(404).send({ message: `Пользователь с таким ID не найден. ${err}` })
