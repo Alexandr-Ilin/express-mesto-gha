a6c03afa04781c9757bf8e0894870e894d0e096d
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-08-10 12:01:34 +0300

    auth-only

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,23 +1,22 @@
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../utils/consts');
+const UnauthtorizedError = require('../utils/errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   // тут будет вся авторизация
   const { jwt: token } = req.cookies;
   if (!token) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new UnauthtorizedError('Необходима авторизация'));
+    return;
   }
   let payload;
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
     // отправим ошибку, если не получилось
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new UnauthtorizedError('Необходима авторизация'));
+    return;
   }
   req.user = payload;
-  return next();
+  next();
 };
