e8a00dafb2827c22678dc0451d7ec986b74861be
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-08-10 13:05:02 +0300

    app-ok

--- app.js
+++ app.js
@@ -3,12 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-const auth = require('./middlewares/auth');
-const { login, createUser } = require('./controllers/users');
-const userRouter = require('./routes/users');
-const cardRouter = require('./routes/cards');
-const { validationCreateUser, validationLogin } = require('./middlewares/validation');
-const NotFoundError = require('./utils/errors/NotFoundError');
+const router = require('./routes/index');
 const { INTERNAL_SERVER_ERROR_STATUS } = require('./utils/consts');
 
 const { PORT = 3000 } = process.env;
@@ -22,17 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
-app.post('/signin', validationLogin, login);
-app.post('/signup', validationCreateUser, createUser);
-
-app.use(auth);
-
-app.use('/users', userRouter);
-app.use('/cards', cardRouter);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundError('Страница не найдена'));
-});
+app.use(router);
 
 app.use(errors());
 
@@ -44,9 +29,7 @@ app.use((err, req, res, next) => {
     .send({
       // проверяем статус и выставляем сообщение в зависимости от него
       message: statusCode === INTERNAL_SERVER_ERROR_STATUS
-        // ? 'На сервере произошла ошибка'
-        ? `На сервере произошла ошибка ${err}`
-        // ? 'На сервере произошла ошибка'
+        ? 'На сервере произошла ошибка'
         : message,
     });
   next();
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const userRouter = require('./users');
+const cardRouter = require('./users');
+const auth = require('../middlewares/auth');
+const NotFoundError = require('../utils/errors/NotFoundError');
+const { login, createUser } = require('../controllers/users');
+const { validationCreateUser, validationLogin } = require('../middlewares/validation');
+
+router.post('/signin', validationLogin, login);
+router.post('/signup', validationCreateUser, createUser);
+
+router.use(auth);
+
+router.use('/users', userRouter);
+router.use('/cards', cardRouter);
+router.use('*', (req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+
+module.exports = router;
