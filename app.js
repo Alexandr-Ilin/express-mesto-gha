7d460310424273bbbf60fe0443b7387980e98bcc
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-08-13 03:27:02 +0300

    cors

--- app.js
+++ app.js
@@ -11,6 +11,21 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
+const allowedCors = [
+  'http://ilin.nomoredomains.sbs',
+  'https://ilin.nomoredomains.sbs',
+  'http://localhost:3000',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
