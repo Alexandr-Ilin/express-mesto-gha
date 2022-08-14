dee7a4e3803aad60e8336522e407870eb991a924
Alexandr-Ilin <ilin-a.a.1981@yandex.ru>
2022-08-14 19:11:38 +0300

    credtrue-

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.use((req, res, next) => {
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
-  res.header({'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true'});
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
